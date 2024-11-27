87775fb1de78a5089ac53cff9198ac88c7458df0
t-drape <thebatreview1@gmail.com>
2024-11-26 19:33:12 -0700

    Start pawn class with tests

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,1 +1,30 @@
+# frozen_string_literal: true
 
+# A class to model a Pawn in chess
+class Pawn
+  def initialize(color, pos)
+    @color = color
+    @position = pos
+  end
+
+  def on_opening?
+    if @color == 'black' && @position[0] == 1
+      return true
+    elsif @color == 'white' && @position[0] == 6
+      return true
+    end
+
+    false
+  end
+
+  def available_moves
+    moves = [1]
+    moves << 2 if on_opening?
+    moves = moves.map(&:-@) if @color == 'white'
+    end_moves = []
+    moves.each do |vert_add|
+      end_moves << [@position[0] + vert_add, @position[1]]
+    end
+    end_moves
+  end
+end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -1,1 +1,62 @@
+# frozen_string_literal: true
 
+require_relative('./../lib/pieces/pawn')
+
+describe Pawn do
+  describe 'on_opening?' do
+    context 'when a pawn is on opening square' do
+      subject(:opener) { described_class.new('black', [1, 3]) }
+
+      it 'returns true for black pawn' do
+        expect(opener.on_opening?).to eql(true)
+      end
+
+      subject(:opener) { described_class.new('white', [6, 3]) }
+      it 'returns true for white pawn' do
+        expect(opener.on_opening?).to eql(true)
+      end
+    end
+    context 'When a pawn is not on the opening square' do
+      subject(:not_opener) { described_class.new('black', [4, 3]) }
+
+      it 'returns false for black pawn' do
+        expect(not_opener.on_opening?).to eql(false)
+      end
+
+      subject(:not_opener) { described_class.new('white', [4, 3]) }
+
+      it 'returns false for white pawn' do
+        expect(not_opener.on_opening?).to eql(false)
+      end
+    end
+  end
+  describe '#available_moves' do
+    context 'when a pawn is on its starting block' do
+      subject(:black_pawn) { described_class.new('black', [1, 3]) }
+      it 'returns single and double openings for black pawn' do
+        expected_output = [[2, 3], [3, 3]]
+        expect(black_pawn.available_moves).to eql(expected_output)
+      end
+
+      subject(:white_pawn) { described_class.new('white', [6, 3]) }
+      it 'returns single and double openings for white pawn' do
+        expected_output = [[5, 3], [4, 3]]
+        expect(white_pawn.available_moves).to eql(expected_output)
+      end
+    end
+
+    context 'when a pawn is not on its starting block' do
+      subject(:black_pawn) { described_class.new('black', [5, 4]) }
+      it 'returns single move for black pawn' do
+        expected_output = [[6, 4]]
+        expect(black_pawn.available_moves).to eql(expected_output)
+      end
+
+      subject(:white_pawn) { described_class.new('white', [5, 4]) }
+      it 'returns single move for white pawn' do
+        expected_output = [[4, 4]]
+        expect(white_pawn.available_moves).to eql(expected_output)
+      end
+    end
+  end
+end
