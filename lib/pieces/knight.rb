f676e56a837cf4d1b5c0323cecab14b06fb42699
t-drape <thebatreview1@gmail.com>
2024-11-30 10:07:39 -0700

    Finish black knight class with tests, Finish white knight class with tests

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,17 +1,94 @@
+# frozen_string_literal: true
+
+# A model for a Black Knight in Chess
 class BlackKnight
-  def initialize(pos, board, last_move)
+  attr_accessor :color, :board
+
+  def initialize(pos, board)
     @color = 'black'
     @pos = pos
     @board = board
-    @last_move = last_move
+  end
+
+  def moves
+    moves = []
+    # Get Left Moves
+    left = left_moves
+    # Get Right Moves
+    right = right_moves
+    # Add Left Moves
+    left&.each { |e| moves << e }
+    # Add Right Moves
+    right&.each { |e| moves << e }
+    moves
+  end
+
+  def left_moves
+    board_range = (0..7)
+    moves = []
+    [[-2, 1], [-1, 2], [1, 2], [2, 1]].each do |y, x|
+      moves << [@pos[0] + y, @pos[1] + x] if board_range.include?(@pos[0] + y) && board_range.include?(@pos[1] + x)
+    end
+    moves
+  end
+
+  def right_moves
+    board_range = (0..7)
+    moves = []
+    [[-2, -1], [-1, -2], [1, -2], [2, -1]].each do |y, x|
+      moves << [@pos[0] + y, @pos[1] + x] if board_range.include?(@pos[0] + y) && board_range.include?(@pos[1] + x)
+    end
+    moves
   end
 end
 
+# A model for a White Knight in Chess
 class WhiteKnight
-  def initialize(pos, board, last_move)
+  attr_accessor :color, :board
+
+  def initialize(pos, board)
     @color = 'white'
     @pos = pos
     @board = board
-    @last_move = last_move
+  end
+
+  def moves
+    moves = []
+    # Get Left Moves
+    left = left_moves
+    # Get Right Moves
+    right = right_moves
+    # Add Left Moves
+    left&.each { |e| moves << e }
+    # Add Right Moves
+    right&.each { |e| moves << e }
+    moves
+  end
+
+  def left_moves
+    board_range = (0..7)
+    moves = []
+    [[-2, -1], [-1, -2], [1, -2], [2, -1]].each do |y, x|
+      moves << [@pos[0] + y, @pos[1] + x] if board_range.include?(@pos[0] + y) && board_range.include?(@pos[1] + x)
+    end
+    moves
+  end
+
+  def right_moves
+    board_range = (0..7)
+    moves = []
+    [[-2, 1], [-1, 2], [1, 2], [2, 1]].each do |y, x|
+      moves << [@pos[0] + y, @pos[1] + x] if board_range.include?(@pos[0] + y) && board_range.include?(@pos[1] + x)
+    end
+    moves
   end
 end
+
+# 1 - row, 2 - spot
+# 2 - row, 1 - spot
+# 2 - row, 1 + spot
+# 1 - row, 2 + spot
+# 1 + row, 2 - spot
+# 2 + row, 1 - spot
+# 2 + row, 1 + spot
+# 1 + row, 2 + spot
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,1 +1,157 @@
+# frozen_string_literal: true
 
+require_relative('./../lib/pieces/knight')
+
+describe BlackKnight do # rubocop:disable Metrics/BlockLength
+  describe '#moves' do # rubocop:disable Metrics/BlockLength
+    context 'when a knight is the chosen piece' do # rubocop:disable Metrics/BlockLength
+      board = [[nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil]]
+      subject(:full_mover) { described_class.new([0, 1], board) }
+      before do
+        allow(full_mover).to receive(:left_moves).and_return([[1, 3], [2, 2]])
+        allow(full_mover).to receive(:right_moves).and_return([[2, 0]])
+      end
+
+      it 'returns the correct moves available from that spot' do
+        full_mover.board[0][1] = full_mover
+        # Start Square being [0, 1]
+        expected_output = [[1, 3], [2, 2], [2, 0]]
+        expect(full_mover.moves).to eql(expected_output)
+      end
+
+      it 'calls left_moves once' do
+        expect(full_mover).to receive(:left_moves).once
+        full_mover.moves
+      end
+      it 'calls right_moves once' do
+        expect(full_mover).to receive(:right_moves).once
+        full_mover.moves
+      end
+    end
+  end
+  describe '#left_moves' do
+    context 'when moves is called for a knight' do
+      board = [[nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil]]
+
+      subject(:mover) { described_class.new([4, 3], board) }
+      it 'returns all moves to the left of the piece' do
+        mover.board[4][3] = mover
+        # From [4, 3]
+        expected_output = [[2, 4], [3, 5], [5, 5], [6, 4]]
+        expect(mover.left_moves).to eql(expected_output)
+      end
+    end
+  end
+
+  describe '#right_moves' do
+    context 'when moves called for a knight' do
+      board = [[nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil]]
+      subject(:mover) { described_class.new([4, 3], board) }
+
+      it 'returns all moves to the right of the piece' do
+        mover.board[4][3] = mover
+        # From [4, 3]
+        expected_output = [[2, 2], [3, 1], [5, 1], [6, 2]]
+        expect(mover.right_moves).to eql(expected_output)
+      end
+    end
+  end
+end
+
+describe WhiteKnight do # rubocop:disable Metrics/BlockLength
+  describe '#moves' do # rubocop:disable Metrics/BlockLength
+    context 'when a knight is the chosen piece' do # rubocop:disable Metrics/BlockLength
+      board = [[nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil]]
+      subject(:full_mover) { described_class.new([0, 1], board) }
+      before do
+        allow(full_mover).to receive(:left_moves).and_return([[1, 3], [2, 2]])
+        allow(full_mover).to receive(:right_moves).and_return([[2, 0]])
+      end
+
+      it 'returns the correct moves available from that spot' do
+        full_mover.board[0][1] = full_mover
+        # Start Square being [0, 1]
+        expected_output = [[1, 3], [2, 2], [2, 0]]
+        expect(full_mover.moves).to eql(expected_output)
+      end
+
+      it 'calls left_moves once' do
+        expect(full_mover).to receive(:left_moves).once
+        full_mover.moves
+      end
+      it 'calls right_moves once' do
+        expect(full_mover).to receive(:right_moves).once
+        full_mover.moves
+      end
+    end
+  end
+  describe '#right_moves' do
+    context 'when moves is called for a knight' do
+      board = [[nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil]]
+
+      subject(:mover) { described_class.new([4, 3], board) }
+      it 'returns all moves to the right of the piece' do
+        mover.board[4][3] = mover
+        # From [4, 3]
+        expected_output = [[2, 4], [3, 5], [5, 5], [6, 4]]
+        expect(mover.right_moves).to eql(expected_output)
+      end
+    end
+  end
+
+  describe '#left_moves' do
+    context 'when moves called for a knight' do
+      board = [[nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil]]
+      subject(:mover) { described_class.new([4, 3], board) }
+
+      it 'returns all moves to the left of the piece' do
+        mover.board[4][3] = mover
+        # From [4, 3]
+        expected_output = [[2, 2], [3, 1], [5, 1], [6, 2]]
+        expect(mover.left_moves).to eql(expected_output)
+      end
+    end
+  end
+end
