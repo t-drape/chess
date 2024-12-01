6d3c81de3fce394052d717bc706a9248eee32011
t-drape <thebatreview1@gmail.com>
2024-12-01 16:10:23 -0700

    Start black rook class with tests, Start white rook class with tests

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -2,13 +2,40 @@
 
 # A model of a Black Rook in Chess
 class BlackRook
-  attr_accessor :color, :on_opening
+  attr_accessor :color, :board, :on_opening
 
   def initialize(pos, board)
     @color = 'black'
     @pos = pos
     @board = board
     @on_opening = true
+  end
+
+  def moves
+    moves = []
+    normal = normal_moves
+    castles = castling
+    normal.each { |e| moves << e } unless normal.empty?
+    castles.each { |e| moves << e } unless castles.empty?
+    moves
+  end
+
+  def normal_moves
+    moves = []
+    vertical = vertical_moves
+    horizontal = horizontal_moves
+    vertical.each { |e| moves << e } unless vertical.empty?
+    horizontal.each { |e| moves << e } unless horizontal.empty?
+    moves
+  end
+
+  def vertical_moves
+  end
+
+  def horizontal_moves
+  end
+
+  def castling
   end
 end
 
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,33 +1,99 @@
 # frozen_string_literal: true
 
 require_relative('./../lib/pieces/rook')
+require_relative('./../lib/pieces/king')
 
-describe BlackRook do
-  describe '#moves' do
-  context "when a rook is selected" do
-    board = [[nil, nil, nil, nil, nil, nil, nil, nil],
-            [nil, nil, nil, nil, nil, nil, nil, nil],
-            [nil, nil, nil, nil, nil, nil, nil, nil],
-            [nil, nil, nil, nil, nil, nil, nil, nil],
-            [nil, nil, nil, nil, nil, nil, nil, nil],
-            [nil, nil, nil, nil, nil, nil, nil, nil],
-            [nil, nil, nil, nil, nil, nil, nil, nil],
-            [nil, nil, nil, nil, nil, nil, nil, nil]]
+describe BlackRook do # rubocop:disable Metrics/BlockLength
+  describe '#moves' do # rubocop:disable Metrics/BlockLength
+    context 'when a rook is selected' do
+      board = [[nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil]]
 
-    subject(:castle) {described_class.new([0,0], board)}
+      subject(:rook_mover) { described_class.new([0, 0], board) }
 
-    allow(castle).to receive(:castling).once
-    
-    it "calls castling once" do
+      before do
+        allow(rook_mover).to receive(:normal_moves).and_return([[1, 1], [2, 0]])
+        allow(rook_mover).to receive(:castling).and_return([[0, 3]])
+      end
 
+      it 'calls castling once' do
+        rook_mover.board[0][0] = rook_mover
+        expect(rook_mover).to receive(:castling).once
+        rook_mover.moves
+      end
+
+      it 'calls normal_moves once' do
+        expect(rook_mover).to receive(:normal_moves).once
+        rook_mover.moves
+      end
+
+      it 'returns the correct moves' do
+        expected_output = [[1, 1], [2, 0], [0, 3]]
+        expect(rook_mover.moves).to eql(expected_output)
+      end
+    end
+  end
+
+  describe '#normal_moves' do
+    context 'when a black rook on square [0, 0] is the chosen piece' do
+      board = [[nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil, nil, nil, nil]]
+
+      subject(:zero_rook) { described_class.new([0, 0], board) }
+
+      before do
+        allow(zero_rook).to receive(:vertical_moves).and_return([[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0],
+                                                                 [7, 0]])
+
+        allow(zero_rook).to receive(:horizontal_moves).and_return([[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6],
+                                                                   [0, 7]])
+      end
+
+      it 'calls vertical_moves once' do
+        zero_rook.board[0][0] = zero_rook
+        expect(zero_rook).to receive(:vertical_moves).once
+        zero_rook.normal_moves
+      end
+
+      it 'calls horizontal_moves once' do
+        expect(zero_rook).to receive(:horizontal_moves).once
+        zero_rook.normal_moves
+      end
+
+      it 'returns the correct moves' do
+        expected_output = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0],
+                           [7, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6],
+                           [0, 7]]
+        expect(zero_rook.normal_moves).to eql(expected_output)
+      end
+    end
+  end
+
+  describe '#vertical_moves' do
+    context 'when the rook at square 0,9 is selected' do
     end
 
-    it "calls normal_moves once" do
+    context 'when the rook at square 0,7 is selected' do
+    end
+  end
 
+  describe '#horizontal_moves' do
+    context 'when the rook at square 0,0 is selected' do
     end
 
-    it "returns the correct moves" do
-      expected_moves = [[1, 0], [2, 0], [3, 0], [0, 1], [0, 2], [0, 3], [0, 4]]
+    context 'when the rook at square 0,7 is selected' do
     end
   end
 end
