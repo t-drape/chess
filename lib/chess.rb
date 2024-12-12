ab4516bbabd6c95a67ffe2792296ae4daa4edf7d
t-drape <thebatreview1@gmail.com>
2024-12-12 15:09:32 -0700

    Finish pseudocode for play game function, Finish psuedocode for end game check function, Finish pseudocode for out of check function, Comment dysfunctional code until player classes are finished, Require player file

--- lib/chess.rb
+++ lib/chess.rb
@@ -3,20 +3,25 @@ require_relative('./../lib/pieces/pawn')
 require_relative('./../lib/pieces/bishop')
 require_relative('./../lib/pieces/knight')
 require_relative('./../lib/pieces/queen')
+require_relative('./player')
 
 class Game
   attr_accessor :board
 
   def initialize
-    @player_one = 'white'
-    @player_two = 'black'
+    @player_one = WhitePlayer.new
+    @player_two = BlackPlayer.new
     @current_player = @player_one
     @winner = nil
-    @board = [["\u265C", "\u265E", "\u265D", "\u265B", "\u265A", "\u265D", "\u265E", "\u265C"],
-              ["\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F"],
+    @board = [[@player_two.rook_one, @player_two.knight_one, @player_two.bishop_one, @player_two.king, @player_two.queen,
+               @player_two.bishop_two, @player_two.knight_two, @player_two.rook_two],
+              [@player_two.pawn_one, @player_two.pawn_two, @player_two.pawn_three, @player_two.pawn_four,
+               @player_two.pawn_five, @player_two.pawn_six, @player_two.pawn_seven, @player_two.pawn_eight],
               [], [], [], [],
-              ["\u2659", "\u2659", "\u2659", "\u2659", "\u2659", "\u2659", "\u2659", "\u2659"],
-              ["\u2656", "\u2658", "\u2657", "\u2655", "\u2654", "\u2657", "\u2658", "\u2656"]]
+              [@player_one.pawn_one, @player_one.pawn_two, @player_one.pawn_three, @player_one.pawn_four,
+               @player_one.pawn_five, @player_one.pawn_six, @player_one.pawn_seven, @player_one.pawn_eight],
+              [@player_one.rook_one, @player_one.knight_one, @player_one.bishop_one, @player_one.king,
+               @player_one.queen, @player_one.bishop_two, @player_one.knight_two, @player_one.rook_two]]
   end
 
   def show_board
@@ -25,19 +30,64 @@ class Game
     end
   end
 
+  def play_game
+    start_message
+    play_round until @winner || draw?
+    end_message(@winner)
+  end
+  # Maybe calculate current players legal moves,
+
+  # Use moves to determine if checkmate exists
+  # All moves = 0 and @current_player.king.in_check?
+  # Winner is opponent
+  # Use moves to determine if stalemate exists
+  # all_moves == 0
+
+  # Helpful site, url: https://www.chessstrategyonline.com/content/tutorials/how-to-play-chess-draws#:~:text=Insufficient%20material,drawn%20due%20to%20insufficient%20material.
+
+  # End game check
+  # Checkmate:
+  # @current_player.all_moves_lead_to_check
+  # end_message(@other_player)
+  # Stalemate:
+  # @current_player.no_legal_moves
+  # end_message(nil)
+  # Insufficient material:
+  # King vs king with no other pieces.
+  # King and bishop vs king.
+  # King and knight vs king.
+  # @current_player vs @other_player
+  # end_message(nil)
+  # Voluntary Draw:
+  # "Player @current_player wishes to draw: "
+  # "Player @other_player,"
+  # "Do you also wish to draw? [Y/N]"
+  # end_message(nil)
+
   def play_round
     # Show board on each round
     show_board
     # Get piece and move from user
     piece, move = select_piece_and_move
     # Update Board
+    # If current king is in check
+    old_board = @board
     update_board(piece, move)
+    # Move must get King out of check, or keep King out of check
+
+    # UNCOMMENT WHEN PLAYERS ARE MODELED!!!
+
+    # out_of_check(old_board)
     # Check if move initiated check
     # check_message if @current_player == @player_one ? @player_two.king.in_check? : @player_one.king_in_check?
-    # Check for stalemate
-    # stalemate
-    # Ask for draw
-    # draw?
+  end
+
+  def out_of_check(old_board)
+    return unless @current_player.king.in_check?
+
+    puts "Sorry! Your move must move the king out of harm's way..."
+    @board = old_board
+    play_round
   end
 
   def update_board(piece, move)
@@ -82,6 +132,7 @@ class Game
     return false if move.length != 2
     return false unless (0..7).include?(move[0])
     return false unless (0..7).include?(move[1])
+    # If move is in current players all move
     return false unless piece.moves.include?(move)
 
     # Make sure move is in piece's available moves!
